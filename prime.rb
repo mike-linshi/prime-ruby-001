df5ac592ebd2aea8020e71a1b0b820b0c71f0913
mike-linshi <mike.linshi@flatironschool.com>
2015-10-12 20:29:46 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+# prime?() returns true for prime numbers
+
+def prime?(num)
+
+  # for i in 1..num, ensure num % i != 0
+  for i in 2..(num-1)
+    remainder = num % i
+    if remainder == 0
+      return false
+      break
+    end
+  end
+  
+  return true
+
+end
